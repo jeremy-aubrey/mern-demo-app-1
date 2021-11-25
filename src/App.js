bceb58df23f3b7dd9976c6ce6f3952e59ab60616
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-24 18:17:22 -0600

    added redirect to /

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 
@@ -12,6 +12,7 @@ const App = () => {
       <Route path="/users" exact>
         <Users />
       </Route>
+      <Redirect to="/" />
     </Router>
   );
 };
