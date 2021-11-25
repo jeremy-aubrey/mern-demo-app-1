136c38823a02ca31ea2f8c4b52959962a38b81fb
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-24 19:19:07 -0600

    generates user list from props data, displays message if no user data found

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -4,7 +4,29 @@ import UserItem from "./UserItem";
 import "./UsersList.module.css";
 
 const UsersList = (props) => {
-    
+  if (props.users.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.users.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default UsersList;
