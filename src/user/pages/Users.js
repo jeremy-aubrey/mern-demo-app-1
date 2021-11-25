28c3566f1fd6bef2742457f768daa6cdaf8a8b17
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-24 19:19:43 -0600

    added dummy data for testing

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h1>Users Page</h1>;
+  const USERS = [
+    {
+      id: "1",
+      name: "Jeremy Aubrey",
+      image:
+        "https://static01.nyt.com/images/2019/06/17/science/17DOGS/17DOGS-mobileMasterAt3x-v2.jpg",
+      places: 10
+    },
+  ];
+
+  return <UsersList users={USERS} />;
 };
 export default Users;
